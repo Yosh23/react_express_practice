b476ef174c88f85313bddcf424b7d46ff3203b95
Josh Riches <jriches@Joshs-MacBook-Pro.local>
2020-04-06 18:12:28 -0600

    connect dashboard & tasklist

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
-console.log("hello world");
-
 import { store } from './store'
-console.log("before state");
-console.log(store.getState());
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Main } from './components/Main'
+
+ReactDom.render(<Main/>,document.getElementById("app"));
