044bd364b54c22a9044622de26979aec05d6bb39
Josh Riches <jriches@Joshs-MacBook-Pro.local>
2020-04-13 19:17:33 -0600

    implemented interaction with task lists and handling state change

--- src/app/components/TaskDetail.js
+++ src/app/components/TaskDetail.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import * as mutations from '../store/mutations';
+
 
 const TaskDetail = ({
     id,
     comments,
     task,
     isComplete,
-    groups
+    groups,
+    setTaskCompletion,
+    setTaskGroup,
+    setTaskName,
 })=>(
     <div>
         <h2>
-            {task.name}
+            <input onChange={setTaskName}value={task.name}/>
         </h2>
         <div>
-            <button> Complete / Reopen Task</button>
+            <button onClick = {()=> setTaskCompletion(id,!isComplete)}>{isComplete ?  `Reopen` : `Complete`}</button>
         </div>
         <div>
-            <select>
+            <select onChange={setTaskGroup}>
                 {groups.map(group=>(<option key={group.id} value={group.id}>{group.name}</option>))}
             </select>
         </div>
@@ -42,4 +47,19 @@ const mapStateToProps = (state,ownProps) => {
     }
 }
 
-export const ConnectedTaskDetail = connect(mapStateToProps)(TaskDetail);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const id = ownProps.match.params.id;
+    return {
+        setTaskCompletion(id,isComplete) {
+            dispatch(mutations.setTaskCompletion(id,isComplete))
+        },
+        setTaskGroup(e) {
+            dispatch(mutations.setTaskGroup(id,e.target.value))
+        },
+        setTaskName(e) {
+            dispatch(mutations.setTaskName(id,e.target.value))
+        }
+    }
+}
+
+export const ConnectedTaskDetail = connect(mapStateToProps, mapDispatchToProps)(TaskDetail);
